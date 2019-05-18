9c4c86666789c298d148f0bb391b8e8074010d8b
Ricardo González Castillo <rjgcastillo@hotmail.com>
2019-05-17 23:45:03 -0500

    Fix some errors in client

--- client.py
+++ client.py
@@ -6,13 +6,14 @@ from json import dumps
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-client_address = (socket.gethostname, 3000)
+host = socket.gethostname()
+port = 3000
 
 # Se conecta con el servidor
-s.connect(client_address)
+s.connect((host, port))
 
 messages = [(0.0, 'RealMemory 2'), (0.1, 'SwapMemory 4'), (0.2, 'PageSize 16'),
-            (0.3, 'PolíticaMemory LRM y MRM'), (1, 'P 2048 1'), (2, 'A 1 1 0'),
+            (0.3, 'PolíticaMemory LRM'), (1, 'P 2048 1'), (2, 'A 1 1 0'),
             (3, 'A 33 1 1'), (4, 'P 32 2'), (5, 'A 15 2 0'), (6, 'A 82 1 0'),
             (7, 'L 2'), (8, 'P 32 3'),  (9, 'L 1'),  (10, 'F'),  (11,  'E')]
 
